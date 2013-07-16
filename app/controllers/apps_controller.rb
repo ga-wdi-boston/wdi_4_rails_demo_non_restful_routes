764050238f4d2dcc5829640964f9265dcc885904
David Fisher <tibbon+github@gmail.com>
2013-07-16 12:53:44 -0400

    Removed public index. Redid reroute files a bit

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -9,4 +9,12 @@ class AppsController < ApplicationController
     @app = App.find(params[:id])
   end
 
+  # POST /apps/:id/buy
+  def buy
+    app = App.find(params[:id])
+    user = User.find(params[:user_id])
+    user.transaction.create(app: app)
+    redirect_to app, notice: "App purchased by #{user.name}"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 AppStore::Application.routes.draw do
-  resources :users
-  resources :apps
+	root to: 'apps#index'
+
+  resources :users, only: [:index, :new, :create, :show] do
+    	resources :apps, only: [:index, :new]
+  end
+
+  resources :apps, only: [:index, :new]
+
+  post '/apps/:id/buy', to: 'apps#buy', as: 'buy_app'
+
+  get '/applications', to: redirect('/apps')
+
 end
