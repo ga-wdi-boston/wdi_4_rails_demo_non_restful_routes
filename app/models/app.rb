01e479cc0c6ea6b761c39947f51f51055185c3e8
David Fisher <tibbon+github@gmail.com>
2013-07-16 11:28:23 -0400

    Added images url to apps

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,3 +1,5 @@
 class App < ActiveRecord::Base
   attr_accessible :description, :name, :price
+  has_many :transactions
+  has_many :users, through: :transactions
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: "Ada Lovelace")
+User.create(name: "Steve Jobs")
+User.create(name: "Bill Gates")
+User.create(name: "Marissa Mayer")
+User.create(name: "Roberta Williams")
+User.create(name: "Jeri Ellsworth")
+User.create(name: "Douglas Hofstadter")
+
+App.create(name: "Angry Birds", price: 2.99, description: "The survival of the Angry Birds is at stake. Dish out revenge on the greedy pigs who stole their eggs. Use the unique powers of each bird to destroy the pigs’ defenses. Angry Birds features challenging physics-based gameplay and hours of replay value. Each level requires logic, skill and force to solve.  ")
+App.create(name: "Google Maps", price: 0.00,description: "Navigate your world with Google Maps, now available for iPhone. Get comprehensive, accurate and easy-to-use maps with built-in Google local search, voice guided turn-by-turn navigation, public transit directions, Street View and more. Use Google Maps to discover great places to eat, drink, shop and play, with ratings and reviews from people you trust. Sign in to save your favorite places and quickly access all your past searches and directions from your computer, right on your phone.")
+App.create(name: "Hipmunk",price: 0.00,description: "Hipmunk is the fastest, easiest way to search for flights and hotels. We save you time and money by comparing all the top travel sites so you don’t have to. Plus, Hipmunk is free.")
+App.create(name: "Minecraft",price: 6.99,description: "Minecraft is about placing blocks to build things and going on adventures.")
+App.create(name: "Plants vs Zombies",price: 0.99,description: "The Zombies are coming. Use your plants to save you.")
